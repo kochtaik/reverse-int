f9e22b617bc03e607c879cb7c49b3d3ebf2fee91
kochtaik <kochtaik@gmail.com>
2020-10-05 14:04:30 +0300

    resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const str = String(n);
+  let reversed = str.split('').reverse().join('');
+  if (reversed[reversed.length - 1] === '-') {
+    reversed = reversed.slice(0, str.length - 1)
+  } return Number(reversed);
 }
